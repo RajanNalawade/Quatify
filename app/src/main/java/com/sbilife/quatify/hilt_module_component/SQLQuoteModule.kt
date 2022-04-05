7e8d36d8a25f69b389376e29531fcb8ab1df9b99
rajan_git_test <raj.nalawade2337@gmail.com>
2022-04-05 18:55:23 +0530

    Hilt Dependancy Injection -
    @Binds - Want to bind Interface with its implementation @Binds annotation is used.
    
    1.Function should be abstract[only function signature] to use @Binds annotation.
    2. so class also becomes abstract.

--- app/src/main/java/com/sbilife/quatify/hilt_module_component/SQLQuoteModule.kt
+++ app/src/main/java/com/sbilife/quatify/hilt_module_component/SQLQuoteModule.kt
@@ -1,18 +1,16 @@
 package com.sbilife.quatify.hilt_module_component
 
-import com.sbilife.quatify.FirebaseRepo
 import com.sbilife.quatify.QuoteRepo
+import com.sbilife.quatify.SQLRepo
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 
 @InstallIn(ActivityComponent::class)
 @Module
-class SQLQuoteModule {
+abstract class SQLQuoteModule {
 
-    @Provides
-    fun provideSQLQuoterepo(): QuoteRepo {
-        return FirebaseRepo()
-    }
+    @Binds
+    abstract fun provideSQLQuoterepo(mSQLRepo: SQLRepo): QuoteRepo
 }
